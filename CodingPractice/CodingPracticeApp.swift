e18d1b873c1ce946a2510a6a0e53761bb1cadb0d
RonJurincie <ronjurincie@gmail.com>
2023-12-19 19:52:22 -0500

    Initial Commit

---
+++ CodingPractice/CodingPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingPracticeApp.swift
+//  CodingPractice
+//
+//  Created by Ron Jurincie on 12/19/23.
+//
+
+import SwiftUI
+
+@main
+struct CodingPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
